338a0891b0e9dee3913be42281e2769ba8a96714
Max <wurzer.maxi@gmx.de>
2021-09-14 17:36:09 +0200

    Update select example
    Signed-off-by: Max <wurzer.maxi@gmx.de>

--- presentation_code/basicsSelect.go
+++ presentation_code/basicsSelect.go
@@ -6,12 +6,24 @@ import (
 )
 
 func main() {
-	resultsChan := make(chan string)
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-	select {
-	case result := <-resultsChan:
-		fmt.Println(result)
-	case <-time.After(1 * time.Second):
-		fmt.Println("Timeout")
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
 	}
 }
